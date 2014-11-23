f3ae71eef4d67040afad1519f71f67fa17384146
mathieu <mathieu@cogimator.net>
2014-11-23 20:00:19 +0100

    First API commit

---
+++ SimpleValidation.Tests/ApiTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SimpleValidation.Builders;
+
+namespace SimpleValidation.Tests
+{
+    [TestClass]
+    public class ApiTests
+    {
+        [TestMethod]
+        public void This_must_compile()
+        {
+            // arrange
+            var sut = Mock.Of<IValidatorBuilder<Person>>();
+
+            // act
+            var rule = sut
+                .RuleFor(x => x.DateOfBirth)
+                    .NotEmpty()
+                    .LessThanToday()
+                .RuleFor(x => x.FirstName)
+                    .NotEmpty()
+                    .NotWhiteSpace()
+                .RuleFor(x => x.LastName)
+                    .NotWhiteSpace();
+        }
+    }
+}
---
+++ SimpleValidation/Builders/IStringRuleBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleValidation.Builders
+{
+    public interface IStringRuleBuilder<TTarget> : IRuleBuilderBase<TTarget, IStringRuleBuilder<TTarget>>
+    {
+        IStringRuleBuilder<TTarget> NotWhiteSpace();
+
+        IStringRuleBuilder<TTarget> Email();
+    }
+}
