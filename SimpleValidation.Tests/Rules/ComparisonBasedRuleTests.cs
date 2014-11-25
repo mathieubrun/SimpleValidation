7f7cb4ed7be620ae1cfe7e0aabafdf93b46613b8
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2014-11-25 21:59:12 +0100

    First rules

---
+++ SimpleValidation.Tests/Rules/ComparisonBasedRuleTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleValidation.Builders;
+using SimpleValidation.Rules;
+
+namespace SimpleValidation.Tests.Rules
+{
+    [TestClass]
+    public class ComparisonBasedRuleTests
+    {
+        [TestMethod]
+        public void Execute_must_return_correct_ValidationResult()
+        {
+            // arrange
+            var date = DateTime.Now;
+            var obj = new Person() { DateOfBirth = date };
+
+            // assert
+            Assert.AreEqual(true, new ComparisonBasedRule<DateTime>(() => obj.DateOfBirth, Comparisons.LessThan, date.AddDays(-1)).Execute().IsSuccess);
+            Assert.AreEqual(true, new ComparisonBasedRule<DateTime>(() => obj.DateOfBirth, Comparisons.LessThanOrEqual, date.AddDays(-1)).Execute().IsSuccess);
+            Assert.AreEqual(true, new ComparisonBasedRule<DateTime>(() => obj.DateOfBirth, Comparisons.LessThanOrEqual, date).Execute().IsSuccess);
+            Assert.AreEqual(true, new ComparisonBasedRule<DateTime>(() => obj.DateOfBirth, Comparisons.Equal, date).Execute().IsSuccess);
+            Assert.AreEqual(true, new ComparisonBasedRule<DateTime>(() => obj.DateOfBirth, Comparisons.GreaterThanOrEqual, date).Execute().IsSuccess);
+            Assert.AreEqual(true, new ComparisonBasedRule<DateTime>(() => obj.DateOfBirth, Comparisons.GreaterThanOrEqual, date.AddDays(1)).Execute().IsSuccess);
+            Assert.AreEqual(true, new ComparisonBasedRule<DateTime>(() => obj.DateOfBirth, Comparisons.GreaterThan, date.AddDays(1)).Execute().IsSuccess);
+        }
+    }
+}
---
+++ SimpleValidation/Rules/ExpressionBasedRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace SimpleValidation.Rules
+{
+    public abstract class ExpressionBasedRule<TProperty> : IRule
+    {
+        private readonly Func<TProperty> getValue;
+
+        public ExpressionBasedRule(Expression<Func<TProperty>> getProperty)
+        {
+            this.getValue = getProperty.Compile();
+        }
+
+        public abstract ValidationResult Execute();
+
+        protected TProperty GetValue()
+        {
+            return getValue();
+        }
+    }
+}
