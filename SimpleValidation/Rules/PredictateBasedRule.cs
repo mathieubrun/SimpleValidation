946e2b9b4fcbc809c1c0c2f5fd2bbcb0c4469d21
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2014-11-30 19:50:58 +0100

    Missing files

---
+++ SimpleValidation/Rules/PredictateBasedRule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace SimpleValidation.Rules
+{
+    public class PredictateBasedRule<TTarget, TProperty> : ExpressionBasedRule<TTarget, TProperty> where TProperty : IComparable, IComparable<TProperty>
+    {
+        private readonly Predicate<TProperty> predicate;
+
+        public PredictateBasedRule(Expression<Func<TTarget, TProperty>> getProperty, Predicate<TProperty> predicate)
+            : base(getProperty)
+        {
+            this.predicate = predicate;
+        }
+
+        public override ValidationResult Execute(TTarget target)
+        {
+            var result = GetValue(target);
+
+            return GetResult(predicate(result));
+        }
+
+        private ValidationResult GetResult(bool success, string message = null)
+        {
+            if (success)
+            {
+                return ValidationResult.Success(message);
+            }
+
+            return ValidationResult.Failure(message);
+        }
+    }
+}
